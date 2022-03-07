c660542fe19571fb716f78f0c1be15b437165a29
Roni Saha <39988759+RoniKumerSaha@users.noreply.github.com>
2022-03-07 15:31:37 +0600

    Added testNG

--- src/test/java/api/tests/BookingsTest.java
+++ src/test/java/api/tests/BookingsTest.java
@@ -4,50 +4,45 @@ import data.Booking;
 import helper.ApiHelper;
 import io.restassured.path.json.JsonPath;
 import io.restassured.response.Response;
-import org.junit.jupiter.api.*;
+import org.testng.Assert;
+import org.testng.annotations.*;
 
 public class BookingsTest {
 
-    @BeforeEach
-    @DisplayName("Setting up the API")
+    @BeforeMethod
     public void setUP(){
         ApiHelper.setUpAPI();
     }
 
 
-    @Test
-    @DisplayName("Verify all booking are present or not")
+    @Test(priority = 2, description = "Checking all existing bookings")
     public void verifyAllBooking(){
         Response res = ApiHelper.GetBookings();
-        Assertions.assertEquals(200, res.statusCode());
-        Assertions.assertTrue(res.getBody().asString().contains("\"bookingid\":9"));
+        Assert.assertEquals(200, res.statusCode());
+        Assert.assertTrue(res.getBody().asString().contains("\"bookingid\":9"));
     }
-    @Test
-    @DisplayName("Verify a single booking")
+    @Test(priority = 1, description = "Verify a booking using booking id")
     public void verifyBooking(){
         Response res = ApiHelper.GetBookingWithID(10);
         JsonPath data = res.jsonPath();
-        Assertions.assertEquals(200, res.statusCode());
-        Assertions.assertEquals("Mark", data.get("firstname"));
-        Assertions.assertEquals("Brown", data.get("lastname"));
-        Assertions.assertEquals(625, (int)data.get("totalprice"));
+        Assert.assertEquals(200, res.statusCode());
+        Assert.assertEquals("Mark", data.get("firstname"));
+        Assert.assertEquals("Brown", data.get("lastname"));
+        Assert.assertEquals(625, (int)data.get("totalprice"));
     }
 
-    @Test
-    @DisplayName("Verify creating a new booking")
+    @Test(priority = 3, description = "Creating a new booking")
     public void verifyCreateBooking(){
         Response response = ApiHelper.createBooking(Booking.bookingData);
         JsonPath resData = response.jsonPath();
-        Assertions.assertEquals(200, response.statusCode());
-        Assertions.assertEquals("Jim", resData.getMap("booking").get("firstname"));
+        Assert.assertEquals(200, response.statusCode());
+        Assert.assertEquals("Jim", resData.getMap("booking").get("firstname"));
     }
 
-    @Test
-    @DisplayName("Verify deleting a booking")
-    @Disabled
+    @Test(priority = 4, description = "Delete a booking")
     public void verifyDeleteBooking(){
         Response response = ApiHelper.deleteBooking(5);
-        Assertions.assertEquals(201, response.statusCode());
+        Assert.assertEquals(201, response.statusCode());
     }
 
 }
